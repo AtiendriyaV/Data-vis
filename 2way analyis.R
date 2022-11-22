c8505d0272556ee074ff45ff14db45a6129dbcc1
AtiendriyaV <65350839+AtiendriyaV@users.noreply.github.com>
2022-11-22 13:51:11 +0530

    Update 2way analyis.R

--- 2way analyis.R
+++ 2way analyis.R
@@ -1,16 +1,17 @@
 library("multcomp")
+library("gplots")
+
 library(readxl)
+
+da <- read_excel("C:\\Users\\student\\Desktop\\blood.xlsx")
+View(da)
 
 dat2 <- cbind(cholesterol,da)
 
 str("dat2")
-
-attach(dat2)
 
 aov2way <- aov(response~trt+BP , data=da)
 
 summary(aov2way)
 
 print(dat2)
-
-detach(dat2)
