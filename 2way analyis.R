74469e8cd0eaed704823c557f51f33d1cec4f230
AtiendriyaV <65350839+AtiendriyaV@users.noreply.github.com>
2022-11-22 13:46:34 +0530

    Update 2way analyis.R

--- 2way analyis.R
+++ 2way analyis.R
@@ -1,8 +1,5 @@
 library("multcomp")
-library("gplots")
-
 library(readxl)
-
 
 dat2 <- cbind(cholesterol,da)
 
@@ -14,12 +11,6 @@ aov2way <- aov(response~trt+BP , data=da)
 
 summary(aov2way)
 
-detach(dat2)
-
 print(dat2)
 
-
-#plotmeans(response~trt+BP, xab="treatment" , ylab="response" , main = "Mean Plot\nwith 95% CI")
-
-#da <- read_excel("C:\\Users\\student\\Desktop\\blood.xlsx")
-#View(da)
+detach(dat2)
